d50cb8fb3ad15e7903131c32f707bea09b8961a4
phat-tech123 <caovinhphat1904@gmail.com>
2024-10-09 17:23:03 +0700

    update

---
+++ ProgramCounter.v
@@ -0,0 +1,11 @@
+module ProgramCounter(AddressMux, address);
+//initialize size of address 
+parameter SIZE = 5;
+
+//initialize port
+input[SIZE-1:0] AddressMux;
+output reg[SIZE-1:0] address;
+
+
+
+endmodule
